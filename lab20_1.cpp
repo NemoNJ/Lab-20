9488f196f9007832b77fcfb10e8942f574760d2e
NemoNJ <nemojirayu@gmail.com>
2024-02-16 17:39:16 +0700

    finish

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -20,21 +20,69 @@ string toUpperStr(string x){
     return y;
 }
 
-void importDataFromFile(){
-
+void importDataFromFile(string filename,vector<string> &names,vector<int> &scores,vector<char> &grades){
+     string textline;
+     ifstream source(filename);
+     while(getline(source,textline)){
+        char name[99];
+        int score1,score2,score3;
+        char format[] = "%[^:]: %d %d %d";
+        sscanf(textline.c_str(),format,name,&score1,&score2,&score3);
+        int scoreSum = score1+score2+score3;
+        grades.push_back(score2grade(scoreSum));
+        scores.push_back(scoreSum) ;
+        names.push_back(name); 
+     }
 }
 
-void getCommand(){
-
+void getCommand(string &command, string &key){
+        cout << "Please input your command: ";
+        cin >> command;
+        string commandUp = toUpperStr(command);
+        if(commandUp == "EXIT" || commandUp =="GRADE" || commandUp == "NAME"){
+            cin.ignore();
+            getline(cin, key);
+        }
 }
 
-void searchName(){
-
+void searchName(vector<string> names,vector<int> scores,vector<char> grades,string key){
+       bool checkfound;
+       cout << "---------------------------------\n";
+       for(unsigned int i = 0;i < names.size();i++){
+          string nameupper = toUpperStr(names[i]);
+          if(key == nameupper){
+               cout << names[i]<<"'s score = "<<scores[i]<<endl;
+               cout << names[i]<<"'s grade = "<<grades[i]<<endl;
+               checkfound = true; 
+          }
+       }
+       
+       if(checkfound == 0) cout << "Cannot found."<<endl;
+       cout << "---------------------------------\n";
 }
 
-void searchGrade(){
-
+void searchGrade(vector<string> &names, vector<int> &scores, vector<char> &grades, string key){
+       bool checkfound;
+       vector<string> namehavegrade;
+       vector<int> havegrade;
+       cout << "---------------------------------\n";
+       for(unsigned int i = 0;i < names.size();i++){
+       
+          if(key.c_str()[0] == grades[i]){
+                namehavegrade.push_back(names[i]);
+                havegrade.push_back(scores[i]);
+                checkfound = true;
+          }
+       }
+       for(unsigned int i = 0;i < namehavegrade.size();i++){
+       cout << namehavegrade[i] <<" ("<< havegrade[i] <<")"<<endl;
+       }
+       
+       if(checkfound == 0) cout << "Cannot found."<<endl;
+       cout << "---------------------------------\n";
 }
+
+
 
 
 int main(){
